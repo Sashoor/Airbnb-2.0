c2636327c296c922584277dd6421fb693b2ebf83
sashoor <saeedashoor.dev@gmail.com>
2022-05-12 21:44:13 +0100

    formatted it

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Image from "next/image"
-import { SearchIcon,
-GlobeAltIcon,
-MenuIcon,
-UserCircleIcon,
-UserIcon
+import {
+    SearchIcon,
+    GlobeAltIcon,
+    MenuIcon,
+    UserCircleIcon,
+    UserIcon
 } from '@heroicons/react/solid'
 
 function Header() {
@@ -19,21 +20,21 @@ function Header() {
             </div>
             {/* Middle-side  search*/}
             <div className="flex md:border-2 rounded-full py-2 md:shadow-sm ">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600"   type="text" placeholder="Start your search" />
+                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600" type="text" placeholder="Start your search" />
                 <SearchIcon className="hidden md:inline h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
 
             {/* Right-side */}
 
-            <div className="flex space-x-4 items-center justify-end text-gray-500">  
-            <p className="hidden md:inline cursor-pointer">Become a host</p>
-            <GlobeAltIcon className="h-6 cursor-pointer"/>
-            <div className="flex items-center border-2 rounded-full space-x-2 p-2">
-            <MenuIcon className="h-6 cursor-pointer"/>
-            <UserCircleIcon className="h-6 cursor-pointer" />
+            <div className="flex space-x-4 items-center justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer" />
+                <div className="flex items-center border-2 rounded-full space-x-2 p-2">
+                    <MenuIcon className="h-6 cursor-pointer" />
+                    <UserCircleIcon className="h-6 cursor-pointer" />
+                </div>
             </div>
-            </div>
-            
+
         </header>
     )
 }
